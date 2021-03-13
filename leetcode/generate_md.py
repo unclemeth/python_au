84280338bd28568acf25cab15b6aab4721ccaf10
UncleMeth <unclemeth@mail.ru>
2021-03-13 20:25:39 +0300

    Fixed some bugs

--- leetcode/generate_md.py
+++ leetcode/generate_md.py
@@ -1,47 +1,45 @@
-
-LTconst = '<!---end templates--->'
-class LTSourse:
-    def __init__(self, title, link, code):
-        self.title = title.rstrip('\n').split('. ')[1]
-        self.link = link.rstrip('\n')
-        self.code = code
-
-    def __str__(self):
-        return 'title = (), link = (), code = ()'.format(self.title, self.link, self.code)
-    def get_md_formated_code(self):
-        return '```python\n{}\n```'.format('\n'.join(map(lambda x: x[:4], self.code)))
+import sys
+LINK_SOLUTION_SEPARATE = '<---end.markdown links--->'
+def get_all_lines_from_file(file_name):
+    file = open(file_name)
+    syp = file.readlines()
+    return syp
+def get_file_content(file_name):
+    file = open(file_name)
+    src = file.read()
+    file.close
+    return src
+def write_to_md_file(file_name, md_solution):
+    file = open(file_name, "w")
+    file.write(md_solution)
+    file.close
+def merge_solution(old_md_links, old_solution, new_md_link, new_solution):
+    return "{}\n{}{}{}\n{}".format(old_md_links,new_md_link,LINK_SOLUTION_SEPARATE,old_solution,new_solution)
+def str(self):
+    return '{}\n{}\n{}'.format(self.title,self.link,self.sourse_code)
+class Task:
+    def __init__(self, title, link, sourse_code):
+        self.title = title.split('. ')[-1].strip('\n')
+        self.link = link.strip('\n')
+        self.sourse_code = sourse_code
+    def get_md_link(self):
+        return '+ [{}](#{})'.format(self.title, self.link[30:(len(self.link)-1)])
     def get_md_title(self):
         return '## {}'.format(self.title)
-    def get_md_solution_link(self):
-        return '+ ({})(#{})'.format(self.title, self.link[9:-1])
-    def get_formated_solution(self):
-        return '{}\n{}\n\n{}\n\n{}\n\n'.format(self.get_md_solution_link(), LTconst)
-def read_all_lines_from_file(file_name):
-    file = open(file_name)
-    result = file.readlines()
-    file.close()
-    return result
-def write_to_md (file_name, data):
-    file = open(file_name, 'w')
-    file.write(data)
-    file.close()
-def read_all_file(file_name):
-    file = open(file_name)
-    result = file.read()
-    file.close()
-    return result
-def merge_solutions(old_solution, new_solution):
-    old_splitted = old_solution.split(LTconst)
-    if len(old_splitted) == 1:
-        return new_solution
-    return '{}{}{}'.format(old_splitted[0], new_solution, old_splitted[1])
-def main(src,dst):
-    in_text = read_all_lines_from_file(src)
-    source = LTSourse(in_text[0], in_text[1], in_text[3:])
-    new_solutions = source.get_formated_solution()
-    old_solutions = read_all_file(dst)
-    result = merge_solutions(old_solutions, new_solutions)
-    write_to_md(dst, result)
+    def get_md_python_solution(self):
+        return '``` python\n{}\n```'.format(
+            ''.join(map(lambda x: x[4:], self.sourse_code)))
+    def get_md_task_content(self):
+        return self.get_md_link(), '{}\n\n{}\n\n{}'.format(self.get_md_title(), self.link, self.get_md_python_solution())
 
+def main(a,b):
+    sourse_lines = get_all_lines_from_file(a)
+    task = Task(sourse_lines[0], sourse_lines[1], sourse_lines[3:])
+    new_md_link, new_md_task = task.get_md_task_content()
+    old_md_links,old_md_task = get_file_content(b).split("<---end.markdown links--->")
+    write_to_md_file(b, merge_solution(old_md_links, old_md_task, new_md_link, new_md_task))
+
+if __name__ == '__main__':
+    main(sys.argv[1], sys.argv[2])
 
 
